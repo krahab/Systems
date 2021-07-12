d3503c9b2ece18a1daadedfad5d8e6e3de93d95b
krahab <58262600+krahab@users.noreply.github.com>
2021-07-12 15:00:37 -0400

    Custom Grid V1

---
+++ CustomGrid/CustomGridMaterial.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class CustomGridMaterial : MonoBehaviour
+{
+	public float gridSize = 1;
+    public Texture texture;
+    private Material material;
+    void Awake(){
+        Generate();
+    }
+    [ContextMenu("Generate")]
+    void Generate(){
+        material = GetComponent<MeshRenderer>().materials[0];
+        material.mainTexture = texture;
+        Vector2 gridScale = new Vector2(transform.localScale.x/gridSize/2f, transform.localScale.z/gridSize/2f);
+        material.mainTextureScale = gridScale;
+    }
+}
